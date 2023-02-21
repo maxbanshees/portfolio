96f59f6411de8e1b6cf768ec7eb21385d22e3d8e
Max Banshees <126015790+maxbanshees@users.noreply.github.com>
2023-02-21 16:20:43 -0500

    Selected works portfolio.

---
+++ styling/menutoggle.js
@@ -0,0 +1,15 @@
+ /* Open nav + toggle button text */
+ 
+ function openMenu() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("seemenu").style.display = "none";
+    document.getElementById("hidemenu").style.display = "block"; 
+  }
+  
+  /* Close + toggle button text */
+
+  function closeMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("seemenu").style.display = "block";
+    document.getElementById("hidemenu").style.display = "none";  
+  }
